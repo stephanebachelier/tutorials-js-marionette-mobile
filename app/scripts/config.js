e37cfd72cfb96a3c5a9d0c8488fa4545b5534d8b
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-07-24 17:02:24 +0200

    chore(bower): update marionette to 2.x.x

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -22,7 +22,7 @@ require.config({
     underscore: '../bower_components/underscore/underscore',
     'handlebars.runtime': '../bower_components/handlebars/handlebars.runtime.amd',
     handlebars: 'vendor/handlebars',
-    marionette: '../bower_components/backbone.marionette/lib/core/amd/backbone.marionette',
+    marionette: '../bower_components/backbone.marionette/lib/core/backbone.marionette',
     'backbone.babysitter': '../bower_components/backbone.babysitter/lib/backbone.babysitter',
     'backbone.wreqr': '../bower_components/backbone.wreqr/lib/backbone.wreqr',
     syphon: '../bower_components/backbone.syphon/lib/amd/backbone.syphon',
