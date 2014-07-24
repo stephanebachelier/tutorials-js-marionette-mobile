9f96cac162c6fb32f6e8049a1f5210ce01750396
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-07-24 17:16:15 +0200

    feat(routes.list): implement route #list

--- app/scripts/controllers/nav.js
+++ app/scripts/controllers/nav.js
@@ -1,13 +1,23 @@
 define([
-  'marionette'
+  'marionette',
+  'data/movies',
+  'models/movies',
+  'views/movielist'
 ],
 
-function (Marionette) {
+function (Marionette, data, Movies, MovieListView) {
   'use strict';
 
   var NavController = Marionette.Controller.extend({
     initialize: function (options) {
       this.region = options.region;
+      this.movies = new Movies(data.d);
+    },
+
+    list: function () {
+      this.region.show(new MovieListView({
+        collection: this.movies
+      }));
     }
   });
 
--- app/scripts/routers/router.js
+++ app/scripts/routers/router.js
@@ -7,6 +7,7 @@ function (Marionette) {
 
   var Router = Marionette.AppRouter.extend({
     appRoutes: {
+      list: 'list'
     }
   });
 
