9e527e8d7e8fe257359687b2c553752777d4ef93
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-07-26 00:35:19 +0200

    test(api.movies): test movie collection builder
    use the data fixtures

---
+++ test/spec/api/movies.spec.js
@@ -0,0 +1,15 @@
+/* jshint expr: true*/
+define([
+  'api/movies',
+  'data/movies'  // our fixtures
+], function (getMovies, data) {
+  'use strict';
+
+  // unit test suite on nav controller
+  describe.only('getMovies helper', function () {
+    it('should have the correct number of elements', function () {
+      var movies = getMovies(data);
+      movies.length.should.eq(8);
+    })
+  });
+});
