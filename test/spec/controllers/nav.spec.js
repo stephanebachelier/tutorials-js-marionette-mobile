681711b95d2e59fa5fa18c4fe364d0206cdbd739
Stéphane Bachelier <stephane.bachelier@gmail.com>
2014-07-28 11:52:31 +0200

    test(nav.controller): add test skeleton with DOM injection

--- test/spec/controllers/nav.spec.js
+++ test/spec/controllers/nav.spec.js
@@ -1,20 +1,58 @@
 /* jshint expr: true*/
-define(['controllers/nav'], function (NavController) {
+define([
+  'marionette',
+  'controllers/nav'
+], function (Marionette, NavController) {
   'use strict';
 
+  // unit test suite on nav controller
   describe('nav controller', function () {
+    var element = document.createElement('div');
+    element.className = 'content';
 
+    // hook running before each test
     beforeEach(function () {
-      this.controller = new NavController();
+      document.body.appendChild(element);
+
+      var region = new Marionette.Region({
+        el: '.content'
+      });
+
+      this.controller = new NavController({
+        region: region
+      });
     });
 
+    // hook running after each test
     afterEach(function () {
-      this.controller.close();
+      this.controller.destroy();
       this.controller = null;
+
+      document.body.removeChild(element);
     });
 
-    it('should implement some tests', function () {
-      true.should.be.true;
+    // tests on list()
+    describe('list()', function () {
+      it('should render the movie list view', function () {
+        // query the after
+        var view = this.controller.list().currentView;
+      });
+
+      it('should render the same number of items', function () {
+
+      });
+    });
+
+    // tests on showMovie()
+    describe('showMovie()', function () {
+      it('should render the movie details view', function () {
+
+      });
+
+      it('should render the expected movie', function () {
+
+      });
     });
   });
+
 });
